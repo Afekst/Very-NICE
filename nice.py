5317d2e7fb6294721dde332de2df7dc25e61fc85
Afeks <afeks98@outlook.com>
2022-02-19 17:36:18 +0200

    define using max hidden neurons

--- nice.py
+++ nice.py
@@ -155,7 +155,7 @@ class NICE(nn.Module):
         Create NN consist of AdditiveCoupling blocks
         :param coupling: number of coupling blocks
         :param in_out_dim: input/output dimension
-        :param mid_dim: number of units in each hidden layer of the coupling block
+        :param max_neurons: maximal number of hidden neurons
         :param hidden: number of hidden layers in each coupling block
         :return: list of coupling blocks
         """
